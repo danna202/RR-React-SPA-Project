398c90ec3b4b80201ff14e1b57746f35b795187f
Danna Bohnhoff <dannab101@gmail.com>
2023-10-08 11:33:08 -0400

    add nav bar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
+const Product = ({ name, description }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  const products = [
+    { id: 1, name: 'Product 1', description: 'Description for Product 1' },
+    { id: 2, name: 'Product 2', description: 'Description for Product 2' },
+    // Add more products here
+  ];
+
+  return (
+    <div>
+      <Navbar />
+      <div className="container mt-4">
+        <div className="row">
+          {products.map((product) => (
+            <div key={product.id} className="col-md-4 mb-3">
+              <Product name={product.name} description={product.description} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ my-react-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+// Navbar.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        Product Page React App
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
